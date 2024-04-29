35489c7f1ac04adda30327fd9817cd6b92b1918d
lrw666666@qq.com <lrw666666@qq.com>
2024-04-29 15:53:28 +0800

    提交新的文档

--- milvus/pic/ResNetEmbeding.py
+++ milvus/pic/ResNetEmbeding.py
@@ -49,7 +49,7 @@ class ResNetEmbeding:
 
 
 if __name__ == '__main__':
-    resnet = ResNetEmbeding("../model/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5")
+    resnet = ResNetEmbeding("../models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5")
     v = resnet.extract_feature(
         "http://127.0.0.1:9000/picture/ILSVRC2012_val_00001195.JPEG?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=PWRF69G12NIGKASVSMIU%2F20240426%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240426T080543Z&X-Amz-Expires=604800&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJQV1JGNjlHMTJOSUdLQVNWU01JVSIsImV4cCI6MTcxNDE2MDQ1MSwicGFyZW50IjoibWluaW9hZG1pbiJ9.cMrwB1mRJhD5kURUr06mC-ffxBC-PvY4aE7NCyOPxy9vA9BJoqGi5CRCPTkDsu700L1_hrhuVsJRYR25ePP4ww&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=495148349161b61c4345e49d4f631f369a3d9b195f11e8110c8dc29112697309")
     print(v)
